ebe816ecacbd133c668592561b652eaec184ada1
jan438 <jan438@users.noreply.github.com>
2024-12-19 11:14:43 +0100

    Initial commit

---
+++ PlaySound.py
@@ -0,0 +1,22 @@
+import wave
+import sys
+import os
+import pyaudio
+CHUNK = 1024
+if sys.platform[0] == 'l':
+    path = "/home/jan/git/PlaySound"
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/PlaySound"
+os.chdir(path)
+filename = "WAV/sound.wav"
+with wave.open(filename, 'rb') as wf:
+    p = pyaudio.PyAudio()
+    try:
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),channels=wf.getnchannels(),rate=wf.getframerate(),output=True)
+    except Exception as error:
+        print("Can not open stream", error)
+    while len(data := wf.readframes(CHUNK)): 
+        stream.write(data)
+    stream.close()
+    p.terminate()
+key = input("Wait")
